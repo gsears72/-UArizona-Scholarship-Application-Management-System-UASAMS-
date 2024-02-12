16a707445dba39e301a98cf5475230cfbc1107c8
TooleConor <117325876+TooleConor@users.noreply.github.com>
2024-02-11 21:03:38 -0700

    updates the table to have pop up windows

--- ScholarshipDonor/models.py
+++ ScholarshipDonor/models.py
@@ -3,12 +3,22 @@ from django.db import models
 # Create your models here.
 class Scholarship:
 
-    def __init__(self, name, maxAward, totalAvail, dateCreated, applicants, awarded):
+    def __init__(self, name, amount, donor, totalAvail, minGPA, major, dateCreated, applicants, awarded, description):
         self.name = name
-        self.maxAward = maxAward        #max amount of money awarded to one candidate
+        self.amount = amount            #max amount of money awarded to one candidate
+        self.donor = donor
         self.totalAvail = totalAvail    #total money to give out 
+        self.minGPA = minGPA            #min gpa
+        self.major = major
         self.dateCreated = dateCreated  
         self.applicants = applicants    #list of type application
         self.awarded = awarded          #list of type application?
+        self.description = description
+        
+class Donor:
 
+    def __init__(self, name, number, email):
+        self.name = name
+        self.number = number
+        self.email = email
     
--- ScholarshipDonor/views.py
+++ ScholarshipDonor/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.db import models
+from django.http import JsonResponse
 from .tempData import tempData
+from .models import Scholarship
 
 # Create your views here.
 
