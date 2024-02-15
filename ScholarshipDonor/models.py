d3298074ff2899bed24afa1c3255059d78067c8d
Glenn Sears <Glenns@arizona.edu>
2024-02-14 18:03:44 -0700

    Database setup

--- ScholarshipDonor/models.py
+++ ScholarshipDonor/models.py
@@ -1,24 +1,36 @@
 from django.db import models
 
 # Create your models here.
-class Scholarship:
+class Scholarship(models.Model):
+    scholarship_name = models.CharField(max_length=100)
+    scholarship_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    donor_full_name = models.CharField(max_length=100)
+    donor_phone_number = models.CharField(max_length=15)  # Assuming phone numbers as strings
+    donor_email = models.EmailField()
+    num_scholarships_available = models.IntegerField()
+    required_majors_or_minors = models.CharField(max_length=200)
+    required_gpa = models.DecimalField(max_digits=3, decimal_places=2)
+    application_deadline = models.DateField()
+    other_requirements = models.TextField()
 
-    def __init__(self, name, amount, donor, totalAvail, minGPA, major, dateCreated, applicants, awarded, description):
-        self.name = name
-        self.amount = amount            #max amount of money awarded to one candidate
-        self.donor = donor
-        self.totalAvail = totalAvail    #total money to give out 
-        self.minGPA = minGPA            #min gpa
-        self.major = major
-        self.dateCreated = dateCreated  
-        self.applicants = applicants    #list of type application
-        self.awarded = awarded          #list of type application?
-        self.description = description
+    def __str__(self):
+        return self.scholarship_name
+
         
-class Donor:
+class Donor(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    security_question_1 = models.CharField(max_length=100)
+    security_answer_1 = models.CharField(max_length=100)
+    security_question_2 = models.CharField(max_length=100)
+    security_answer_2 = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    net_id = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=15)  # Assuming phone numbers as strings
+    email = models.EmailField()
 
-    def __init__(self, name, number, email):
-        self.name = name
-        self.number = number
-        self.email = email
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.username}"
+
     
