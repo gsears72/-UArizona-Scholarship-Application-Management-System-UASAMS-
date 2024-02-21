1d1aefcaafbc3fbea7eb5b8aee497bbff92ccbd4
Glenn Sears <Glenns@arizona.edu>
2024-02-21 16:46:12 -0700

    Finished All User Register Pages

--- Login/urls.py
+++ Login/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('logout_user', views.logout_user, name='logout'),
 
     path('student_register', views.studentRegister, name='student_register'),
-    #path('scholorship_adminstrator_register', views.scholorshipadminstratoRegister, name='scholorship_adminstrator_register'),
-    #path('scholorship_donor_register', views.scholorshipdonorregisterRegister, name='scholorship_donor_register'),
-    #path('applicant_reviewer_register', views.applicantreviewerRegister, name='applicant_reviewer_register'),
+    path('scholorship_adminstrator_register', views.scholorshipadminstratoRegister, name='scholorship_adminstrator_register'),
+    path('scholorship_donor_register', views.scholorshipdonorregisterRegister, name='scholorship_donor_register'),
+    path('applicant_reviewer_register', views.applicantreviewerRegister, name='applicant_reviewer_register'),
 
 ]
--- Login/views.py
+++ Login/views.py
@@ -3,10 +3,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-from .forms import CreateStudentForm
+from .forms import CreateStudentForm, CreateScholorshipAdministratorForm, CreateScholorshipDonorForm, CreateApplicantReviewerForm
 
 # Create your views here.
 def login_user(request):
+    """
+    Logs in a user.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
     if request.method == 'POST':
         email = request.POST["Email"]
         password = request.POST["Password"]
@@ -22,14 +32,36 @@ def login_user(request):
 
     else:
         return render(request, 'authenticate/login.html',{})
-    
+
+
+
 def logout_user(request):
+    """
+    Logs out the user and redirects to the home page.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - A redirect response to the home page.
+    """
     messages.success(request,'You Were Logged Out')
     logout(request)
     return redirect('home')
 
 
+
 def studentRegister(request):
+    """
+    View function for student registration.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
     form = CreateStudentForm()
     if request.method == 'POST':
          form = CreateStudentForm(request.POST)
@@ -51,28 +83,101 @@ def studentRegister(request):
 
 
 
-
-
-
-
-
 def scholorshipadminstratoRegister(request):
+    """
+    View function for registering a scholarship administrator.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
+    form = CreateScholorshipAdministratorForm()
     if request.method == 'POST':
-        pass
+         form = CreateScholorshipAdministratorForm(request.POST)
+         if form.is_valid():
+             form.save()
+             email = form.cleaned_data.get('email')
+             raw_password = form.cleaned_data.get('password')
+             user = authenticate(email=email, password=raw_password)
+             login(request, user)
+             messages.success(request,'Account Created') 
+             return redirect("home")
+         else:
+                context = {'form': form}
+                return render(request,'authenticate/scholarship_administrator_register.html',context)
     else:
-        return redirect('login')
-    
+        form = CreateScholorshipAdministratorForm()
+        context = {'form': form}
+        return render(request,'authenticate/scholarship_administrator_register.html',context)
+
+
+
+
 def scholorshipdonorregisterRegister(request):
+    """
+    Register a scholarship donor.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
+    form = CreateScholorshipDonorForm()
     if request.method == 'POST':
-        pass
+         form = CreateScholorshipDonorForm(request.POST)
+         if form.is_valid():
+             form.save()
+             email = form.cleaned_data.get('email')
+             raw_password = form.cleaned_data.get('password')
+             user = authenticate(email=email, password=raw_password)
+             login(request, user)
+             messages.success(request,'Account Created') 
+             return redirect("home")
+         else:
+                context = {'form': form}
+                return render(request,'authenticate/scholarship_donor_register.html',context)
     else:
-        return redirect('login')
+        form = CreateScholorshipDonorForm()
+        context = {'form': form}
+        return render(request,'authenticate/scholarship_donor_register.html',context)
+    
+
+    
     
 def applicantreviewerRegister(request):
+    """
+    View function for registering an applicant reviewer.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response object.
+
+    """
+    form = CreateApplicantReviewerForm()
     if request.method == 'POST':
-        pass
+         form = CreateApplicantReviewerForm(request.POST)
+         if form.is_valid():
+             form.save()
+             email = form.cleaned_data.get('email')
+             raw_password = form.cleaned_data.get('password')
+             user = authenticate(email=email, password=raw_password)
+             login(request, user)
+             messages.success(request,'Account Created') 
+             return redirect("home")
+         else:
+                context = {'form': form}
+                return render(request,'authenticate/applicant_reviewer_register.html',context)
     else:
-        return redirect('login')
+        form = CreateApplicantReviewerForm()
+        context = {'form': form}
+        return render(request,'authenticate/scholarship_donor_register.html',context)
     
 
 
