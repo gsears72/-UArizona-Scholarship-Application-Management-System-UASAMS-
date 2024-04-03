203a7edd5e8afedb092f9428c02d2824b9b29ab1
mgcarlson <mgcarlson@arizona.edu>
2024-04-03 15:11:30 -0700

    sprint 5 push

--- ApplicantReviewer/urls.py
+++ ApplicantReviewer/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('SearchApplicants', views.SearchApplicants, name='SearchApplicants'),
     path('MyReviewedApps', views.MyReviewedApps, name='MyReviewedApps'),
     path('Approved', views.Approved, name='Approved'),
-    path ('ViewScholarshipApplicants/<int:scholarship_id>/', views.ViewScholarshipApplicants, name='ViewScholarshipApplicants'),
-    path('ReviewApplication/<int:application_id>/<int:scholarship_id>/', views.ViewScholarshipApplicants, name='ReviewApplication')
+    path('ViewApplicants/<int:scholarship_id>/', views.ViewApplicants, name='ViewApplicants'),
+    path('ViewEligibleApplicants/<int:scholarship_id>/', views.ViewEligibleApplicants, name='ViewEligibleApplicants'),
+    path('ReviewApplication/<int:application_id>/<int:scholarship_id>/', views.ReviewApplication, name='ReviewApplication'),
+    path('application-list', views.application_list, name='application_list'),
+    path('review-submit/<int:application_id>/', views.review_submit, name='review_submit')
 ]
