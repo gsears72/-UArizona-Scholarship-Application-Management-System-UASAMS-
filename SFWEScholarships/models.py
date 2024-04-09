88de3ee46a403773e90511298cb60ffcf81969fd
mgcarlson <mgcarlson@arizona.edu>
2024-04-09 13:51:21 -0700

    commiting my stuff

--- SFWEScholarships/models.py
+++ SFWEScholarships/models.py
@@ -1,7 +1,7 @@
+from Student.models import Student
 from django.db import models
 from ScholarshipDonor.models import Scholarship
 from Student.models import Student
-
 # Create your models here.
 class Application(models.Model):
     STATUS = (
@@ -10,11 +10,20 @@ class Application(models.Model):
         ('Approved', 'Approved'), 
         ('Rejected', 'Rejected')
     )
-
+    
+    SR_STATUS = (
+        ('In Reivew', 'In Review'),
+        ('Approved', 'Approved'), 
+        ('Rejected', 'Rejected')
+    )
+    
+    
+    
 
     scholarship = models.ForeignKey(Scholarship, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     stauts = models.CharField(max_length = 20, choices = STATUS, default = 'Submitted')
-
-    def __str__(self):
-        return f"Application for {self.scholarship.scholarship_name} by {self.student.username}"
+    personal_statement = models.TextField()
+    sr_status = models.CharField(max_length = 20, choices = SR_STATUS, default = 'In Review')
+    score = models.IntegerField(max_length = 3, default = 0)
+    
