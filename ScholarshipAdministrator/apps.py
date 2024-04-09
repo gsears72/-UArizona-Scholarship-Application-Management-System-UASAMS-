644c78540bb7cfb45e98d9d77433cdd2257b7012
glenns <glenns@arizona.edu>
2024-04-09 12:34:43 -0700

    cleared cahch

--- ScholarshipAdministrator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScholarshipadministratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ScholarshipAdministrator'
