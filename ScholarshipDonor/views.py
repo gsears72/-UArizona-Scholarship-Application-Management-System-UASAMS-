659ac41980c21e7b5558cedee767a65fc09b8c98
Grace <gkshah01@arizona.edu>
2024-04-25 13:32:15 -0700

    donor filter

--- ScholarshipDonor/views.py
+++ ScholarshipDonor/views.py
@@ -6,9 +6,13 @@ from django.template.loader import render_to_string
 from django.db import models
 from django.http import JsonResponse
 from .models import Scholarship
+from ScholarshipDonor.models import Donor
 
 # Create your views here.
 
 def home(request):
     scholarships = Scholarship.objects.all()
-    return render(request,'SDhome.html',{})
+    currentUser = request.user
+    donor = get_object_or_404(Donor, donor_info_id = currentUser.id)
+    context = {'scholarships' : scholarships, 'donor' : donor}
+    return render(request,'SDhome.html',context)
